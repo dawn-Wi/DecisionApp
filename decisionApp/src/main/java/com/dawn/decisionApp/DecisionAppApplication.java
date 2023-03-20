4f51949045def3f86c69e6c78d3d8e1c5df11536
dawn <wi825@naver.com>
2023-03-20 18:03:30 +0900

    food (no image) finish

---
+++ decisionApp/src/main/java/com/dawn/decisionApp/DecisionAppApplication.java
@@ -0,0 +1,13 @@
+package com.dawn.decisionApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecisionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecisionAppApplication.class, args);
+	}
+
+}
---
+++ decisionApp/src/test/java/com/dawn/decisionApp/DecisionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dawn.decisionApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecisionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
